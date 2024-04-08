ee99b55d8a91ad3457630a7c52c457866f7cc4cc
Lorenzo Pichetti <lori.pichi@gmail.com>
2024-04-08 11:39:27 +0200

    repo clean

--- sourceFile.sh
+++
@@ -1,7 +0,0 @@
-# Please, source this file each time you open a new session
-SbM_HOME=/home/lorenzo.pichetti/vectorAddTest/SbatchMan ; export SbM_HOME
-SbM_SOUT=/home/lorenzo.pichetti/vectorAddTest/SbatchMan/sout ; export SbM_SOUT
-SbM_UTILS=/home/lorenzo.pichetti/vectorAddTest/SbatchMan/utils ; export SbM_UTILS
-SbM_SBATCH=/home/lorenzo.pichetti/vectorAddTest/SbatchMan/sbatchscripts ; export SbM_SBATCH
-SbM_EXPTABLE=/home/lorenzo.pichetti/vectorAddTest/SbatchMan/metadata/marzola/expTable.csv ; export SbM_EXPTABLE
-SbM_METADATA_HOME=/home/lorenzo.pichetti/vectorAddTest/SbatchMan/metadata ; export SbM_METADATA_HOME
