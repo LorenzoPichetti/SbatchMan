cd681d99f327b8e85105f4e44c8cb47dc45793f3
sasso0101 <git@sasso.dev>
2025-07-03 18:00:26 +0200

    added delete_jobs to api

--- src/sbatchman/__init__.py
+++ src/sbatchman/__init__.py
@@ -5,7 +5,8 @@ __version__ = "0.1.0"
 from .api import init_project, \
     create_local_config, create_slurm_config, create_pbs_config, \
     launch_job, jobs_list, jobs_df, \
-    archive_jobs
+    archive_jobs, \
+    delete_jobs
 from .exceptions import SbatchManError, ProjectNotInitializedError, ProjectExistsError
 from .core.launcher import Job
 
@@ -28,4 +29,5 @@ __all__ = [
     "jobs_df",
 
     "archive_jobs",
+    "delete_jobs",
 ]
