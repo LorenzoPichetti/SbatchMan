467713a31a220083afd879f4eef5202e718cc711
sasso0101 <git@sasso.dev>
2025-07-04 20:20:07 +0200

    Refactor log screen to use Job object for log retrieval and update mkdocs configuration

--- src/sbatchman/tui/log_screen.py
+++ src/sbatchman/tui/log_screen.py
@@ -3,7 +3,8 @@ from textual.widgets import Header, Footer, Log, Markdown
 from textual.containers import Vertical
 from textual.binding import Binding
 from textual.screen import Screen
-from pathlib import Path
+
+from sbatchman.core.job import Job
 
 
 class LogScreen(Screen):
@@ -12,9 +13,9 @@ class LogScreen(Screen):
     Binding("q", "app.pop_screen", "Back to jobs"),
   ]
 
-  def __init__(self, exp_dir: Path, **kwargs):
+  def __init__(self, job: Job, **kwargs):
     super().__init__(**kwargs)
-    self.exp_dir = exp_dir
+    self.job = job
 
   def compose(self) -> ComposeResult:
     yield Header()
@@ -30,18 +31,15 @@ class LogScreen(Screen):
     stdout_log = self.query_one("#stdout_log", Log)
     stderr_log = self.query_one("#stderr_log", Log)
     
-    stdout_path = self.exp_dir / "stdout.log"
-    stderr_path = self.exp_dir / "stderr.log"
+    stdout = self.job.get_stdout()
+    stderr = self.job.get_stderr()
 
-    if stdout_path.exists():
-      with open(stdout_path, "r") as f:
-        stdout_log.write(f.read())
+    if stdout:
+      stdout_log.write(stdout)
     else:
       stdout_log.write(f"No stdout log file found.")
 
-    if stderr_path.exists():
-      with open(stderr_path, "r") as f:
-        stderr_log.write(f.read())
+    if stderr:
+      stderr_log.write(stderr)
     else:
       stderr_log.write(f"No stderr log file found.")
-
